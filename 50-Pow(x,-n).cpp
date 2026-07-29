fb064964f13a646dc16492fdbe47ab4e478f2382
Ridham1210 <rg.hingladiya123@gmail.com>
2026-07-29 17:29:21 +0530

    [50] [Time Beats: 100.00%] [Memory Beats: 93.46%] - LeetPush

---
+++ 50-Pow(x,-n).cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int t) {
+        double ans = 1;
+        long long n = t;
+        if(n < 0){
+            x = 1/x;
+            n = -n;
+        }
+        while(n > 0){
+            if(n %2 != 0){
+                ans *= x;
+                n--;
+            }
+            n /= 2;
+            x *= x;
+        }
+        return ans;
+    }
+};
