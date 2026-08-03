26f932e408f28b91ac98bbd7d88d8a4bd8c914f3
Ridham1210 <rg.hingladiya123@gmail.com>
2026-08-03 13:32:13 +0530

    [992] [Time Beats: 27.59%] [Memory Beats: 18.76%] - LeetPush

---
+++ 992-Subarrays-with-K-Different-Integers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int Count(vector<int>& nums, int k){
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        int i=0, j = 0;
+        int count = 0;
+        int arr = 0;
+        while(j < n){
+            mp[nums[j]]++;
+            while(mp.size() > k){
+                mp[nums[i]]--;
+                if(mp[nums[i]] == 0) mp.erase(nums[i]);
+                i++;
+            }
+            if(mp.size() == k) arr++;
+            count += (j-i+1);
+            j++;
+        }
+        return count;
+    }
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return (Count(nums,k) - Count(nums,k-1));
+    }
+};
